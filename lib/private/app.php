a070629a272ff2e60a630be61ad6e7fcb1ddce38
Robin Appelman <icewind@owncloud.com>
2013-12-30 17:06:52 +0100

    Suppress errors coming from coming getEnabledApps multiple times in test cases

--- lib/private/app.php
+++ lib/private/app.php
@@ -173,19 +173,21 @@ class OC_App{
 		if(!empty(self::$enabledAppsCache)) {
 			return self::$enabledAppsCache;
 		}
-		$apps=array('files');
-		$sql = 'SELECT `appid` FROM `*PREFIX*appconfig`'
-			.' WHERE `configkey` = \'enabled\' AND `configvalue`=\'yes\'';
-		if (OC_Config::getValue( 'dbtype', 'sqlite' ) === 'oci') {
-			//FIXME oracle hack: need to explicitly cast CLOB to CHAR for comparison
-			$sql = 'SELECT `appid` FROM `*PREFIX*appconfig`'
-			.' WHERE `configkey` = \'enabled\' AND to_char(`configvalue`)=\'yes\'';
-		}
-		$result = OC_DB::executeAudited($sql);
-		while($row=$result->fetchRow()) {
-			if(array_search($row['appid'], $apps)===false) {
-				$apps[]=$row['appid'];
+		$apps = array('files');
+		$sql = 'SELECT `appid`, `configvalue` FROM `*PREFIX*appconfig`'
+			.' WHERE `configkey` = \'enabled\'';
+		/**
+		 * @var OC_DB_StatementWrapper $result
+		 */
+		try{
+			$result = OC_DB::executeAudited($sql);
+			while($row = $result->fetchRow()) {
+				if($row['configvalue'] === 'yes' and array_search($row['appid'], $apps)===false) {
+					$apps[] = $row['appid'];
+				}
 			}
+		} catch(DatabaseException $e) {
+
 		}
 		self::$enabledAppsCache = $apps;
 		return $apps;
